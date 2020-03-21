a8a2dfd0c5e33546b615ba5d4d623cc18fa44a53
Legion <pawel.kapusta@student.uj.edu.pl>
2020-03-21 11:21:31 +0100

    Second commit

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -4,6 +4,7 @@ package com.example.demo;
 
 import com.example.demo.dao.FlightRepository;
 import com.example.demo.dao.TouristRepository;
+import com.example.demo.model.Flight;
 import com.example.demo.model.Tourist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -28,12 +29,15 @@ public class DataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Tourist tourist1 = new Tourist("Adam","Kowalski","Male","Poland",
-                "Ma zielone oczy",LocalDate.parse("1968-04-02"));
-        Tourist tourist2 = new Tourist("Monika","Kowalski","Male","Poland",
-                "Ma zielone oczy",LocalDate.parse("1968-04-02"));
+                "Has green eyes",LocalDate.parse("1968-04-02"));
+        Tourist tourist2 = new Tourist("Monika","Czapka","Female","Germany",
+                "Has blond straight hair",LocalDate.parse("1999-12-10"));
 
         touristRepository.save(tourist1);
         touristRepository.save(tourist2);
+
+        flightRepository.save(new Flight(LocalDate.parse("2020-08-12T10:00"),
+                LocalDate.parse("2020-08-12T12:20"),120,125.50));
 
 
     }
--- src/main/java/com/example/demo/model/Tourist.java
+++ src/main/java/com/example/demo/model/Tourist.java
@@ -14,6 +14,7 @@ public class Tourist {
     @GeneratedValue
     private Long id;
 
+
     public void setId(Long id) {
         this.id = id;
     }
